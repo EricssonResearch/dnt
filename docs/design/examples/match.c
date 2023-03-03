7a096419928d44444eb3c04df8f3f1279582ac48
Ferenc Fejes <ferenc.fejes@ericsson.com>
2023-03-03 16:46:13 +0100

    doc: examples: match.c update with print and match functions
    Typed protocol style match and print functions.
    Proper header handlin WIP.

--- docs/design/examples/match.c
+++ docs/design/examples/match.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 
 enum proto_types {
 	ETH,
@@ -18,8 +20,8 @@ enum eth_fields {
 };
 
 struct eth_hdr {
-	uint64_t dmac : 48;
-	uint64_t smac : 48;
+	uint8_t dmac[6];
+	uint8_t smac[6];
 	uint16_t type : 16;
 } __attribute__((packed, scalar_storage_order("big-endian")));
 
@@ -86,44 +88,131 @@ size_t header_size(int proto)
 }
 
 struct header {
+	off_t off;
+	enum proto_types proto_type;
 	struct protocol *proto;
-	char *data;
 };
 
 struct packet;
+struct value;
 
 struct protocol {
-	bool (*match)(struct packet *p);
+	bool (*match)(const char *p, const struct value *v);
 	void (*edit)(struct packet *p);
 	void (*push)(struct packet *p, size_t index);
 	void (*pop)(struct packet *p, size_t index);
 	enum proto_types (*get_next_type)(struct packet *p, size_t index);
 	void (*set_next_type)(struct packet *p, enum proto_types proto);
+	struct value (*value_from_str)(const char *conf);
+	void (*print)(const char *h);
+
 };
 
-
-struct fields_values {
-	off_t offset;
+// store header data for field edit/match
+struct value {
 	uint16_t select;
 	char hdr_data[40];
-	struct protocol *proto;
 };
 
-bool eth_match(struct packet *p)
+struct value *eth_val_from_str(const char *conf)
+{
+	struct value *v = calloc(1, sizeof(struct value));
+	struct eth_hdr *h = (struct eth_hdr *)&v->hdr_data;
+	struct fields {
+		const char *field;
+		enum eth_fields key;
+	} fk[] = {
+		{ "smac", ETH_SMAC },
+		{ "dmac", ETH_DMAC },
+		{ "type", ETH_TYPE }
+	};
+	for(int i = 0; i < sizeof(fk)/sizeof(struct fields); ++i) {
+		const char *conf_field = strstr(conf, fk[i].field);
+		if(!conf_field)
+			continue;
+		v->select |= fk[i].key;
+		switch(fk[i].key) {
+			case ETH_DMAC: {
+				uint8_t *mac = h->dmac;
+				sscanf(conf_field, "dmac=%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", mac, mac+1, mac+2, mac+3, mac+4, mac+5);
+				break;
+			}
+			case ETH_SMAC: {
+				uint8_t *mac = h->smac;
+				sscanf(conf_field, "smac=%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", mac, mac+1, mac+2, mac+3, mac+4, mac+5);
+				break;
+			}
+			case ETH_TYPE: break;
+			default: printf("ERROR, no such ETH field\n"); //exit program
+		}
+	}
+	return v;
+}
+
+bool eth_match(const char *p, const struct value *v)
 {
 	bool match = true;
+	const struct eth_hdr *eh = (struct eth_hdr *) p;
+	const struct eth_hdr *ehv = (struct eth_hdr *) v->hdr_data;
+	if(v->select & ETH_DMAC)
+		match &= (eh->dmac == ehv->dmac);
+	if(v->select & ETH_SMAC)
+		match &= (eh->smac == ehv->smac);
+	return match;
 };
+
+void eth_print(const char *h)
+{
+	const struct eth_hdr *eh = (struct eth_hdr *)h;
+	printf("smac=%hhx:%hhx:%hhx:%hhx:%hhx:%hhx ", eh->smac[0], eh->smac[1], eh->smac[2], eh->smac[3],eh->smac[4], eh->smac[5]);
+	printf("dmac=%hhx:%hhx:%hhx:%hhx:%hhx:%hhx ", eh->dmac[0], eh->dmac[1], eh->dmac[2], eh->dmac[3],eh->dmac[4], eh->dmac[5]);
+	printf("type: %x\n", eh->type);
+}
+
 
 struct packet {
 	struct header hdrs[10];
-	size_t num_of_hdrs;
-	char *data;
-	char *scratch;
-	char buff[2000];
+	size_t num_hdrs;
+	off_t off_data;
+	off_t off_scratch;
+	//char buff[2000];
+	char *buff;
 };
+
+static struct protocol eth_proto = {
+	.match = eth_match,
+	.print = eth_print,
+
+};
+
+void packet_print(const struct packet *p)
+{
+	struct protocol *proto;
+	for (int i = 0; i < p->num_hdrs; ++i) {
+		proto = p->hdrs->proto;
+		const struct header *h = &p->hdrs[i];
+		proto->print(&p->buff[h->off]);
+	}
+}
 
 
 int main()
 {
+	char data[2000] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0b, 0x08, 0x00, /*MAC hdr*/
+			0x81, 0x00, 0x71, 0x01, /*CVLAN*/
+			0x45, 0x00, 0x00, 0x25, 0x77, 0x76, 0x40, 0x00, 0x40, 0x11, 0xc5, 0x4f, 0x7f, 0x00, 0x00, 0x01, 0x7f, 0x00, 0x00, 0x01, /*IP hdr*/
+			0xa2, 0xcb, 0x15, 0xb3, 0x00, 0x11, 0xfe, 0x24, /* UDP hdr */
+			0x64, 0x65, 0x61, 0x64, 0x62, 0x65, 0x65, 0x66, 0x0a}; /* payload */
+	struct packet pkt;
+	pkt.buff = data;
+	pkt.num_hdrs = 1;
+	pkt.hdrs[0].off = 0;
+	pkt.hdrs[0].proto_type = ETH;
+	pkt.hdrs[0].proto = &eth_proto;
+
+	packet_print(&pkt);
+
+	struct value *my_eth = eth_val_from_str("dmac=00:00:00:00:00:ee smac=00:00:00:00:00:cc");
+	eth_print(my_eth->hdr_data);
 }
 
