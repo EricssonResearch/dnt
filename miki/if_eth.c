bce375dbc0a997f5e5c0c6adbc030f95549458ac
Miklós Máté <mate@tmit.bme.hu>
2023-03-24 16:35:56 +0100

    if_eth improvements
    Kernel RX timestamping, add default cvlan to untagged frames, send with
    priority, add bpf filter for priority packets coming back

--- miki/if_eth.c
+++ miki/if_eth.c
@@ -17,13 +17,59 @@
 
 #include <linux/if_ether.h> /* ETH_P_ALL */
 #include <linux/if_packet.h> /* struct sockaddr_ll, PACKET_AUXDATA TODO netpacket/packet.h? */
+#include <linux/net_tstamp.h> /* SOF_TIMESTAMPING_* */
+//#include <linux/sockios.h> /* SIOCSHWTSTAMP */
+#include <linux/filter.h> /* eBPF */
 
 struct EthIfData {
     int ifindex;
     int sockfd[8];
-    bool pcp_used[8]; //TODO we can't filter this
+    bool pcp_used[8]; // indicate whether we have a socket open for this priority
     int mtu;
 };
+
+// copied from the code of the other TSN project
+static void enable_rx_tstamp(int sock, const char *sockname,
+        const char *ifname/*, enum hwtstamp_rx_filters filter*/)
+{
+/*    struct ifreq hwtstamp;
+    struct hwtstamp_config hwconfig, hwconfig_req;
+    memset(&hwtstamp, 0, sizeof(hwtstamp));
+    strncpy(hwtstamp.ifr_name, ifname, sizeof(hwtstamp.ifr_name)-1);
+    hwtstamp.ifr_data = (void *)&hwconfig;
+    memset(&hwconfig, 0, sizeof(hwconfig));
+    hwconfig.tx_type = HWTSTAMP_TX_OFF;
+    hwconfig.rx_filter = filter;
+    hwconfig_req = hwconfig;
+    if (ioctl(sock, SIOCSHWTSTAMP, &hwtstamp) < 0) {
+        if (errno == EINVAL || errno == ENOTSUP) {
+            printf("SIOCSHWTSTAMP: HW timestamping for '%s' on '%s' is not available\n",
+                    sockname, ifname);
+        } else {
+            perror("SIOCSHWTSTAMP");
+        }
+    } else {
+        printf("SIOCSHWTSTAMP: tx_type requested %d got %d, rx_filter requested %d got %d\n",
+                hwconfig_req.tx_type, hwconfig.tx_type,
+                hwconfig_req.rx_filter, hwconfig.rx_filter);
+    }*/
+
+    int so_timestamping_flags =
+        // generate these timestamps
+    //    SOF_TIMESTAMPING_RX_HARDWARE |
+        SOF_TIMESTAMPING_RX_SOFTWARE |
+        // report these timestamps in cmsg
+    //    SOF_TIMESTAMPING_RAW_HARDWARE |
+        SOF_TIMESTAMPING_SOFTWARE |
+        0;
+    if (setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPING,
+                &so_timestamping_flags, sizeof(so_timestamping_flags)) < 0) {
+        perror("setsockopt SO_TIMESTAMPING");
+    } else {
+        printf("setsockopt SO_TIMESTAMPING success for '%s' on '%s'\n",
+                sockname, ifname);
+    }
+}
 
 static struct Packet *eth_recv(struct Interface *iface)
 {
@@ -50,7 +96,7 @@ static struct Packet *eth_recv(struct Interface *iface)
     //printf("recvmsg %d controllen %zu\n", res, msg.msg_controllen);
     if (res < 0) {
         perror("recvmsg");
-        return NULL;
+        return delete_packet(p);
     }
 
     if (packet_dummy(p)) {
@@ -61,20 +107,31 @@ static struct Packet *eth_recv(struct Interface *iface)
         return delete_packet(p);
     }
 
-    //TODO ask for kernel RX timestamping and use that
-    //  ask for both HW and SW, if HW==0 use SW (?)
-    clock_gettime(CLOCK_REALTIME, &p->recv_time);
-
     if (res > 0) {
         p->len = res;
-        //TODO what else?
     }
     printf("eth %s recv %u\n", iface->name, p->len);
 
-    // process the cmsg to get the vlan header info
+    // process the cmsg to get the vlan header info and timestamp
     // if we have vlan, restore it in the packet
     for (struct cmsghdr *cmsg=CMSG_FIRSTHDR(&msg); cmsg; cmsg=CMSG_NXTHDR(&msg, cmsg)) {
         switch (cmsg->cmsg_level) {
+            case SOL_SOCKET:
+                if (cmsg->cmsg_type == SCM_TIMESTAMPING) {
+                    struct timespec *tstamp;
+                    if (cmsg->cmsg_len < sizeof(struct cmsghdr)+3*sizeof(struct timespec)) {
+                        fprintf(stderr, "cmsg_len %zu tstamp %zu cmsghdr %zu\n",
+                                cmsg->cmsg_len, sizeof(struct timespec), sizeof(struct cmsghdr));
+                    } else {
+                        // we aligned msg.msg_control so the alignment should be okay here
+                        tstamp = (struct timespec *)CMSG_DATA(cmsg);
+                        printf("RX SW %ld.%09ld HW %ld.%09ld\n",
+                                tstamp[0].tv_sec, tstamp[0].tv_nsec,
+                                tstamp[2].tv_sec, tstamp[2].tv_nsec);
+                        p->recv_time = tstamp[0];
+                    }
+                }
+                break;
             case SOL_PACKET:
                 if (cmsg->cmsg_type == PACKET_AUXDATA) {
                     struct tpacket_auxdata *aux = (struct tpacket_auxdata *)CMSG_DATA(cmsg);
@@ -95,6 +152,18 @@ static struct Packet *eth_recv(struct Interface *iface)
         }
     }
 
+    uint16_t *p_vlan = (uint16_t*)(p->buf + p->start + 2*6);
+    unsigned short ethertype = ntohs(*p_vlan);
+    if (ethertype != ETH_P_8021Q && ethertype != ETH_P_8021AD) {
+        printf("adding default cvlan to untagged packet\n");
+        memmove(p->buf + p->start - 4, p->buf + p->start, 2*6);
+        p->start -= 4;
+        p->len += 4;
+        p_vlan = (uint16_t*)(p->buf + p->start + 2*6);
+        p_vlan[0] = htons(ETH_P_8021Q);
+        p_vlan[1] = 0;
+    }
+
     return p;
 }
 
@@ -103,11 +172,11 @@ static bool eth_send(struct Interface *iface, struct Packet *p)
     struct EthIfData *eid = iface->iface_private;
 
     if (p->header_count < 1) {
-        fprintf(stderr, "eth send: packet doesn't have headers\n");
+        fprintf(stderr, "eth %s send: packet doesn't have headers\n", iface->name);
         return false;
     }
     if (p->headers[0].type != PROTO_ID_ETH) {
-        fprintf(stderr, "eth send: first header of the packet is not eth\n");
+        fprintf(stderr, "eth %s send: first header of the packet is not eth\n", iface->name);
         return false;
     }
 
@@ -120,8 +189,13 @@ static bool eth_send(struct Interface *iface, struct Packet *p)
     memcpy(socket_address.sll_addr, dst_mac, ETH_ALEN);
 
     unsigned pcp = 0;
-    //TODO if p->header_count > 1 && p->headers[1].type is cvlan or svlan)
-    //          get pcp from that header
+    if (p->header_count > 1) {
+        if (p->headers[1].type == PROTO_ID_CVLAN || p->headers[1].type == PROTO_ID_SVLAN) {
+            unsigned char *vlan = p->buf + p->headers[1].start;
+            pcp = (*vlan) >> 5;
+        }
+    }
+    printf("eth %s sending with priority %u\n", iface->name, pcp);
 
     struct msghdr msg;
     memset(&msg, 0, sizeof(msg));
@@ -129,8 +203,7 @@ static bool eth_send(struct Interface *iface, struct Packet *p)
     msg.msg_namelen = sizeof(socket_address);
 
     //TODO optimization: merge headers if h[i+1].start = h[i].start+h[i].len
-    //TODO struct iovec iov[p->header_count] if this compiles
-    struct iovec *iov = calloc_struct_array(iovec, p->header_count);
+    struct iovec iov[PACKET_MAX_HEADER_NUM];
     for (unsigned i=0; i<p->header_count; i++) {
         iov[i].iov_base = p->buf + p->headers[i].start;
         iov[i].iov_len = p->headers[i].len;
@@ -140,11 +213,9 @@ static bool eth_send(struct Interface *iface, struct Packet *p)
 
     if (sendmsg(eid->sockfd[pcp], &msg, 0) < 0) {
         perror("eth sendmsg");
-        free(iov);
         return false;
     }
 
-    free(iov);
     return true;
 }
 
@@ -176,6 +247,7 @@ static bool eth_open(struct Interface *iface)
         }*/
         if (setsockopt(sock, SOL_SOCKET, SO_PRIORITY, &i, sizeof(i)) < 0) {
             perror("setsockopt SO_PRIORITY");
+            close(sock);
             return false; //TODO cleanup on error
         }
 
@@ -210,6 +282,8 @@ static bool eth_open(struct Interface *iface)
                 return false;
             }
 
+            enable_rx_tstamp(sock, "eth rx", iface->ifname/*, HWTSTAMP_FILTER_ALL*/);
+
             // bind to device
             struct sockaddr_ll socket_address = {0};
             socket_address.sll_family = AF_PACKET;
@@ -230,9 +304,22 @@ static bool eth_open(struct Interface *iface)
                 return false;
             }
 
-            //TODO the old code also installs a BPF filter:
-            //  Block all PACKET_OUTGOING packets sent on other priority sockets
-            //  do we need that?
+            // Block all PACKET_OUTGOING packets sent on other priority sockets from
+            // coming back on socket 0
+            struct sock_filter filter[] = {
+                BPF_STMT(BPF_LD|BPF_B|BPF_ABS, (uint32_t)(SKF_AD_OFF + SKF_AD_PKTTYPE)),
+                BPF_JUMP(BPF_JMP|BPF_K|BPF_JEQ, PACKET_OUTGOING, 0, 0x1),
+                BPF_STMT(BPF_RET|BPF_K, 0x00000000),
+                BPF_STMT(BPF_RET|BPF_K, 0x0000ffff)
+            };
+            struct sock_fprog bpf = {
+                .len = (unsigned short) (sizeof(filter) / sizeof(filter[0])),
+                .filter = filter
+            };
+            if (setsockopt(sock, SOL_SOCKET, SO_ATTACH_FILTER, &bpf, sizeof(bpf)) < 0) {
+                perror("setsockopt attach eBPF filter");
+                return false;
+            }
         } else {
             // set socket receive buffer to minimum (setting to zero will set it to minimum)
             int recvbuf_len = 0;
