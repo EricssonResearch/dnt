1abf93e641d6f356d9b582d5049ac8d133d96248
Ferenc Fejes <ferenc.fejes@ericsson.com>
2023-03-30 16:34:21 +0200

    match: if no pipeline found, continue the recv_loop and dont tread packet as if matched

--- miki/main.c
+++ miki/main.c
@@ -73,15 +73,15 @@ static void recv_loop(struct Interface *ifaces, unsigned iface_count)
                 continue;
             printf("received packet length %u on %s\n", p->len, recvif->name);
             struct Pipeline *pipe = parsetree_process(recvif->parsetree, p);
-            printf("parsetree identified %u headers, pipe = %p\n", p->header_count, pipe);
-            for (unsigned i=0; i<p->header_count; i++) {
-                printf("  header %u is %s at %u len %u\n", i,
-                        protocol_list[p->headers[i].type].name, p->headers[i].start, p->headers[i].len);
-            }
             if (pipe == NULL) {
                 fprintf(stderr, "no pipeline found for packet on %s, unknown stream\n", recvif->name);
                 delete_packet(p);
             } else {
+                printf("parsetree identified %u headers, pipe = %p\n", p->header_count, pipe);
+                for (unsigned i=0; i<p->header_count; i++) {
+                    printf("  header %u is %s at %u len %u\n", i,
+                            protocol_list[p->headers[i].type].name, p->headers[i].start, p->headers[i].len);
+                }
                 // the iterator owns the packet
                 struct PipelineIterator *pi = new_pipe_iterator(pipe, p);
                 // the iterator deletes itself when it's done
