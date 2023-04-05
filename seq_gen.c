e96e8e3d9b5a344408ed96dbd212c8ee5dcf6b71
Ferenc Fejes <ferenc.fejes@ericsson.com>
2023-04-05 17:54:15 +0200

    Add normal/seamless seq generator

--- seq_gen.c
+++ seq_gen.c
@@ -1,8 +1,10 @@
 
 #include "seq_gen.h"
+#include "seq_recov.h"
 #include "packet.h"
 #include "utils.h"
 
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -12,24 +14,26 @@
 struct SequenceGenerator {
     bool use_reset_flag;
     bool use_init_flag;
-    unsigned init_seq;
+    unsigned init_seq_start;
 
-    unsigned seq;
-    bool reset;
-    bool init;
+    unsigned gen_seq_num;
+    unsigned init_gen_seq_num;
+    bool reset_flag;
+    bool init_seq_space;
 
+    unsigned resets;
 };
 
-struct SequenceGenerator *new_seq_gen(bool use_reset_flag, bool use_init_flag, unsigned init_seq)
+struct SequenceGenerator *new_seq_gen(bool use_reset_flag, bool use_init_flag, unsigned init_seq_start)
 {
     struct SequenceGenerator *ret = calloc_struct(SequenceGenerator);
     ret->use_reset_flag = use_reset_flag;
     ret->use_init_flag = use_init_flag;
-    ret->init_seq = init_seq;
+    ret->init_gen_seq_num = init_seq_start;
 
-    ret->seq = init_seq;
-    ret->reset = use_reset_flag;
-    ret->init = use_init_flag;
+    ret->gen_seq_num = init_seq_start;
+    ret->reset_flag = use_reset_flag;
+    ret->init_seq_space = use_init_flag;
 
     return ret;
 }
@@ -41,19 +45,45 @@ struct SequenceGenerator *delete_seq_gen(struct SequenceGenerator *gen)
 }
 
 
-static void step_seq(struct SequenceGenerator *gen)
+void sequence_generation_reset(struct SequenceGenerator *gen)
 {
-    gen->seq++;
-    //TODO manage overflow, flags etc.
+    gen->reset_flag = gen->use_reset_flag;
+    gen->init_seq_space = gen->use_init_flag;
+
+    gen->gen_seq_num = 0;
+    gen->init_gen_seq_num = gen->init_seq_start;
+    gen->resets += 1;
+}
+
+
+static unsigned sequence_generation(struct SequenceGenerator *gen)
+{
+    if (gen->init_seq_space) {
+        unsigned seq = gen->init_gen_seq_num;
+        if(gen->init_gen_seq_num > (FRER_RCVY_SEQ_SPACE - 1)) {
+            gen->gen_seq_num = 1;
+            gen->init_seq_space = false;
+            gen->init_gen_seq_num = gen->init_seq_start;
+        } else {
+            gen->init_gen_seq_num += 1;
+            if (gen->init_gen_seq_num > gen->init_seq_start + FRER_SEQ_GEN_RESET_FLAG_COUNT) {
+                gen->reset_flag = false;
+            }
+        }
+        return seq;
+    }
+    //regular seq generator
+    unsigned seq = gen->gen_seq_num;
+    if (gen->gen_seq_num >= (FRER_RCVY_SEQ_SPACE - 1)) {
+        gen->gen_seq_num = 0;
+    } else {
+        gen->gen_seq_num += 1;
+    }
+    return seq;
 }
 
 void seq_generator(struct SequenceGenerator *gen, struct Packet *p)
 {
-    uint32_t seqn = 0;
-
-    //printf("seq gen %u 0x%x\n", gen->seq, gen->seq);
-    seqn = htonl(gen->seq); //TODO add gen->flags
-    p->sequence = seqn;
-
-    step_seq(gen);
+    unsigned new_seq = sequence_generation(gen);
+    p->sequence = htonl(new_seq);
 }
--- seq_gen.h
+++ seq_gen.h
@@ -6,6 +6,8 @@
 
 #include <stdbool.h>
 
+#define FRER_SEQ_GEN_RESET_FLAG_COUNT 3
+
 struct SequenceGenerator;
 
 struct SequenceGenerator *new_seq_gen(bool use_reset_flag, bool use_init_flag, unsigned init_seq);
@@ -16,5 +18,7 @@ struct SequenceGenerator *delete_seq_gen(struct SequenceGenerator *gen);
 // this is a Producer function
 // @state is struct SequenceGenerator
 void seq_generator(struct SequenceGenerator *gen, struct Packet *p);
+
+void sequence_generation_reset(struct SequenceGenerator *gen);
 
 #endif // R2_SEQ_GEN_H
