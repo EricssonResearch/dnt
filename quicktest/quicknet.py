869018989730ae0c0bbdbb67c139213a7d40ca96
Miklós Máté <mate@tmit.bme.hu>
2023-04-13 11:47:49 +0200

    quicktest: add IPv6

--- quicktest/quicknet.py
+++ quicktest/quicknet.py
@@ -27,32 +27,45 @@ def start_network():
     # config addresses, disable tx checksum offload
     na.cmd("ip a a 10.0.1.1/24 dev ethAB; ethtool -K ethAB tx off")
     nb.cmd("ip a a 10.0.1.2/24 dev ethBA; ethtool -K ethBA tx off")
+    na.cmd("ip a a fd01::1/64 dev ethAB")
+    nb.cmd("ip a a fd01::2/64 dev ethBA")
 
     nb.cmd("ip a a 10.0.2.2/24 dev ethBC; ethtool -K ethBC tx off")
     nc.cmd("ip a a 10.0.2.3/24 dev ethCB; ethtool -K ethCB tx off")
+    nb.cmd("ip a a fd02::2/64 dev ethBC")
+    nc.cmd("ip a a fd02::3/64 dev ethCB")
 
     nc.cmd("ip a a 10.0.3.3/24 dev ethCD; ethtool -K ethCD tx off")
     nd.cmd("ip a a 10.0.3.4/24 dev ethDC; ethtool -K ethDC tx off")
+    nc.cmd("ip a a fd03::3/64 dev ethCD")
+    nd.cmd("ip a a fd03::4/64 dev ethDC")
 
     nd.cmd("ip a a 10.0.4.4/24 dev ethDA; ethtool -K ethDA tx off")
     na.cmd("ip a a 10.0.4.1/24 dev ethAD; ethtool -K ethAD tx off")
+    nd.cmd("ip a a fd04::4/64 dev ethDA")
+    na.cmd("ip a a fd04::1/64 dev ethAD")
 
     na.cmd("ip l a link ethAB name ethAB.r2tunnel type vlan id 1001")
     na.cmd("ip a a 192.168.1.1/24 dev ethAB.r2tunnel")
+    na.cmd("ip a a fd92::1/64 dev ethAB.r2tunnel")
     na.cmd("ip l set up dev ethAB.r2tunnel")
     nc.cmd("ip l a link ethCB name ethCB.r2tunnel type vlan id 2023")
     nc.cmd("ip a a 192.168.1.3/24 dev ethCB.r2tunnel")
+    nc.cmd("ip a a fd92::3/64 dev ethCB.r2tunnel")
     nc.cmd("ip l set up dev ethCB.r2tunnel")
     nd.cmd("ip l a link ethDC name ethDC.r2tunnel type vlan id 3210")
     nd.cmd("ip a a 192.168.1.4/24 dev ethDC.r2tunnel")
+    nd.cmd("ip a a fd92::4/64 dev ethDC.r2tunnel")
     nd.cmd("ip l set up dev ethDC.r2tunnel")
 
     #TODO on nb: ../r2dtwo quicktest.ini
     #TODO on na: ping 192.168.1.3
+    #TODO on na: ping fd92::3
 
     #TODO on nb: ../r2dtwo quickdetB.ini
     #TODO on nc: ../r2dtwo quickdetC.ini
     #TODO on na: ping 192.126.1.4
+    #TODO on na: ping fd92::4
 
     return net
 
