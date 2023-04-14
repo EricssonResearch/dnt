2dd849220a8efff99999f7822d397ff63651acef
Ferenc Fejes <ferenc.fejes@ericsson.com>
2023-04-14 13:57:22 +0200

    test: seq: Add sequence recovery reset test

--- test/sequence.py
+++ test/sequence.py
@@ -29,6 +29,10 @@ def config_ifaces():
 
     # accept local ARP on listener uni
     ret += exec_fg(f"sysctl -w net.ipv4.conf.to_r2br1.accept_local=1").returncode
+    ret += exec_fg("ip addr flush dev to_r2br0").returncode
+    ret += exec_fg("ip addr flush dev to_r2br1").returncode
+    ret += exec_fg("ip addr add 10.0.0.1/24 dev to_r2br0").returncode
+    ret += exec_fg("ip addr add 10.0.0.2/24 dev to_r2br1").returncode
     if ret > 0:
         print("Error(s) during interface config. Running without sudo?")
         exit(1)
@@ -36,27 +40,54 @@ def config_ifaces():
 def ping():
     try:
         print("Test simple ping 1k packets...")
-        exec_fg("ip addr add 10.0.0.1/24 dev to_r2br0")
-        exec_fg("ip addr add 10.0.0.2/24 dev to_r2br1")
         exec_bg("../r2dtwo sequence/r2br0.ini")
         exec_bg("../r2dtwo sequence/r2br1.ini")
         time.sleep(1)
 
         num_pings = 1000
         pingcmd = exec_fg(f"ping -I to_r2br0 10.0.0.2 -i 0.0001 -c {num_pings}", timeout=5.0)
-        if f", {num_pings} received," not in pingcmd.stdout[-1000:]:
+        if pingcmd.stdout and f", {num_pings} received," not in pingcmd.stdout:
             print(pingcmd.stdout)
             return 0
 
         print("Test simple ping 70k packets...")
         num_pings = 70000
         pingcmd = exec_fg(f"ping -I to_r2br0 10.0.0.2 -i 0.0001 -c {num_pings}", timeout=10.0)
-        if f", {num_pings} received," not in pingcmd.stdout[-1000:]:
+        if pingcmd.stdout and f", {num_pings} received," not in pingcmd.stdout:
             print(pingcmd.stdout)
             return 0
     except:
             return 0
     return 1
+
+def reset():
+    try:
+        print("Test sequence recovery reset...")
+        r2 = exec_bg("../r2dtwo sequence/r2br0.ini")
+        exec_bg("../r2dtwo sequence/r2br1.ini")
+        time.sleep(1)
+
+        num_pings = 10
+        pingcmd = exec_fg(f"ping -I to_r2br0 10.0.0.2 -i 0.0001 -c {num_pings}", timeout=5.0)
+        if pingcmd.stdout and f", {num_pings} received," not in pingcmd.stdout:
+            print(pingcmd.stdout)
+            return 0
+
+        r2.terminate()
+        # Trigger the recovery reset
+        time.sleep(5)
+        exec_bg("../r2dtwo sequence/r2br0.ini")
+        time.sleep(1)
+
+        num_pings = 10
+        pingcmd = exec_fg(f"ping -I to_r2br0 10.0.0.2 -i 0.0001 -c {num_pings}", timeout=5.0)
+        if pingcmd.stdout and f", {num_pings} received," not in pingcmd.stdout:
+            print(pingcmd.stdout)
+            return 0
+    except:
+        return 0
+    return 1
+
 
 
 def main():
@@ -64,7 +95,7 @@ def main():
     create_ifaces()
     config_ifaces()
     ret = 0
-    tests = [ping]
+    tests = [ping, reset]
     for test in tests:
         ret += test()
         exec_fg("killall r2dtwo")
