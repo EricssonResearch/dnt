8fa3a5baac908f70ebec587931b40f7346899a9a
Ferenc Fejes <ferenc.fejes@ericsson.com>
2023-04-14 13:53:36 +0200

    utils.h: Add timespec compare/add/sub functions from glibc private APIs

--- utils.h
+++ utils.h
@@ -2,6 +2,8 @@
 #ifndef R2_UTILS_H
 #define R2_UTILS_H
 
+#include <time.h>
+
 #define calloc_struct(T) (struct T *)calloc(1, sizeof(struct T))
 
 #define calloc_struct_array(T, n) (struct T *)calloc((n), sizeof(struct T))
@@ -28,5 +30,54 @@
 
 // @src cannot be NULL!
 void *memdup(const void *src, unsigned size);
+
+// timespec manipulator helpers
+// source: https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/pthread/posix-timer.h
+static inline int
+__attribute ((always_inline, unused))
+timespec_compare (const struct timespec *left, const struct timespec *right)
+{
+  if (left->tv_sec < right->tv_sec)
+    return -1;
+  if (left->tv_sec > right->tv_sec)
+    return 1;
+
+  if (left->tv_nsec < right->tv_nsec)
+    return -1;
+  if (left->tv_nsec > right->tv_nsec)
+    return 1;
+
+  return 0;
+}
+
+static inline void
+__attribute ((always_inline, unused))
+timespec_add (struct timespec *sum, const struct timespec *left,
+	      const struct timespec *right)
+{
+  sum->tv_sec = left->tv_sec + right->tv_sec;
+  sum->tv_nsec = left->tv_nsec + right->tv_nsec;
+
+  if (sum->tv_nsec >= 1000000000)
+    {
+      ++sum->tv_sec;
+      sum->tv_nsec -= 1000000000;
+    }
+}
+
+static inline void
+__attribute ((always_inline, unused))
+timespec_sub (struct timespec *diff, const struct timespec *left,
+	      const struct timespec *right)
+{
+  diff->tv_sec = left->tv_sec - right->tv_sec;
+  diff->tv_nsec = left->tv_nsec - right->tv_nsec;
+
+  if (diff->tv_nsec < 0)
+    {
+      --diff->tv_sec;
+      diff->tv_nsec += 1000000000;
+    }
+}
 
 #endif // R2_UTILS_H
