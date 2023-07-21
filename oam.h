c3a8d22452bdd1283197b5b41b9d69aef38292ae
moldovan <moldovan@moldovan-Latitude-E7450.com>
2023-07-21 10:39:31 +0200

    Repaired oam.c and h

--- oam.h
+++ oam.h
@@ -11,7 +11,6 @@
 #include "if_oam_cmd.h"
 #include "interface.h"
 #include "pipeline.h"
-#include "seq_recov.h"
 
 struct Oam {
     struct Interface *if_oam_cmd;
@@ -27,6 +26,9 @@ int oam_ping(unsigned id, char *stream, char *mep_start, char *mep_stop, int lev
 int oam_trace(unsigned id, char *stream, char *mep_start, char *mep_stop, int level);
 int oam_discovery(unsigned id, char *stream, char *mep_start, char *mep_stop, int level);
 int oam_send_reply(char *address, char *msg);
+int oam_recv_reply(char *msg);
+int oam_command_loop(int cmd_fd);
+
 
 void oam_create_mep_start(const char *stream_name, const char *mep_name, int level, unsigned idx);
 void oam_set_pipeline_for_mep_start(const char *stream_name, struct Pipeline *pipe);
@@ -35,8 +37,5 @@ void set_oam_cmd_if(struct Interface *iface);
 void add_oam_if(struct Interface *iface);
 struct Interface *get_oam_cmd_if(const char *name);
 struct Interface *get_oam_if(const char *name);
-
-struct SequenceRecovery *get_oam_rcvy(char *session_id);
-void delete_oam_rcvy(char *session_id);
 
 #endif // R2_OAM_H
