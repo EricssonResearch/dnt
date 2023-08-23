bb36e039080f60f0511c3bb79d4e567544688478
Ferenc Fejes <ferenc.fejes@ericsson.com>
2023-08-23 13:35:35 +0200

    testing: stress: add OAM while stresstesting

--- test/stress.py
+++ test/stress.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+from socket import AF_INET, SOCK_STREAM, SocketType, socket
+from mininet.net import Mininet
+from threading import Thread
+from mininet.cli import CLI
 from utils import *
 import signal
 import time
@@ -8,84 +12,92 @@ import sys
 # LONG_RUN_DURATION_SEC = 24 * 60 * 60
 LONG_RUN_DURATION_SEC = 30
 
-stdouts = { }
-
-nics = [
-    ["to_r2br0", "r2br0_uni"], 
-    ["to_r2br1", "r2br1_uni"],
-    ["r2br0_nni0", "r2br1_nni0"],
-    ["r2br0_nni1", "r2br1_nni1"],
-]
-
-def create_ifaces():
-    for nicpair in nics:
-        exec_fg(f"ip link add {nicpair[0]} type veth peer name {nicpair[1]}")
-
-def cleanup_ifaces():
-    for nicpair in nics:
-        exec_fg(f"ip link del {nicpair[0]} type veth peer name {nicpair[1]}")
-
-def config_ifaces():
-    ret = 0
-    for nic in sum(nics, []):
-        ret += exec_fg(f"sysctl -w net.ipv6.conf.{nic}.disable_ipv6=1").returncode
-        ret += exec_fg(f"ip link set dev {nic} up").returncode
-
-    # up loopback in case its running in namespace
-    ret += exec_fg(f"ip link set dev lo up").returncode
-    # accept local ARP on listener uni
-    ret += exec_fg(f"sysctl -w net.ipv4.conf.to_r2br1.accept_local=1").returncode
-    ret += exec_fg("ip addr flush dev to_r2br0").returncode
-    ret += exec_fg("ip addr flush dev to_r2br1").returncode
-    ret += exec_fg("ip addr add 10.0.0.1/24 dev to_r2br0").returncode
-    ret += exec_fg("ip addr add 10.0.0.2/24 dev to_r2br1").returncode
-    if ret > 0:
-        print("Error(s) during interface config. Running without sudo?")
-        exit(1)
-
 # Tell if ping ICMP sequences in order (true) or not (false)
 def ping_check_out_of_order(ping_output):
     seqs_str = [s for s in ping_output.splitlines() if "icmp_seq" in s]
     seqs = [int(s.split(" ")[4].split("=")[1]) for s in seqs_str]
     return all(s == s_prev + 1 for s_prev, s in zip(seqs, seqs[1:]))
 
-def long_run():
-    print(f"Run R2DTWOs for {LONG_RUN_DURATION_SEC} seconds...")
-    exec_bg("../r2dtwo stress/r2br0.ini", out=OUT_NONE)
-    exec_bg("../r2dtwo stress/r2br1.ini", out=OUT_NONE)
-    time.sleep(1)
-    ping = exec_bg("ping -I to_r2br0 10.0.0.2 -q -A", out=OUT_PIPE)
-    time.sleep(LONG_RUN_DURATION_SEC)
+def long_run(net):
+    t, l, a, b = [net.get(n) for n in ["t", "l", "a", "b"]]
+    rtwo1 = a.popen(f"../r2dtwo stress/a.ini")
+    rtwo2 = b.popen(f"../r2dtwo stress/b.ini")
+    time.sleep(2)
+    # CLI(net)
+    switch_netns("t")
+    ping = exec_bg("ping 192.168.1.2 -q -A", out=OUT_PIPE)
+    switch_netns("a")
+    with socket(AF_INET, SOCK_STREAM, 0) as sock:
+        sock.connect(("10.0.0.1", 8000))
+        now = time.time()
+        stop = now + LONG_RUN_DURATION_SEC
+        while now < stop:
+            sock.sendall("ping s1:start1 any 4 -r -o".encode())
+            time.sleep(2)
+            now = time.time()
+
     ping.send_signal(signal.SIGINT)
+    rtwo1.terminate()
+    rtwo2.terminate()
     ping_out = str(ping.communicate()[0])
     ping_out = ping_out.split('\n')
     for line in ping_out:
         if "transmitted" in line and "received" in line:
+            print(line)
             params = line.split(" ")
-            sent, recvd = params[0], params[3]
-            if sent != recvd:
-                print(line)
+            sent, recvd = int(params[0]), int(params[3])
+            if abs(sent - recvd) >= 2:
                 return 0
     return 1
 
 
+
+def create_net():
+    net = Mininet(autoStaticArp=True)
+    t = net.addHost("t", ip='192.168.1.1/24')
+    l = net.addHost("l", ip='192.168.1.2/24')
+    a = net.addHost("a", ip=None)
+    b = net.addHost("b", ip=None)
+    net.addLink(t, a, intfName1='eth0', intfName2='eth0')
+    net.addLink(l, b, intfName1='eth0', intfName2='eth0')
+    net.addLink(a, b, intfName1='ab0', intfName2='ba0')
+    net.addLink(a, b, intfName1='ab1', intfName2='ba1')
+    net.build()
+    t.cmd("ethtool -K eth0 tx off rx off")
+    l.cmd("ethtool -K eth0 tx off rx off")
+    a.cmd("sysctl -w net.ipv4.ip_forward=1")
+    b.cmd("sysctl -w net.ipv4.ip_forward=1")
+    a.cmd("ip a a 10.0.0.1/32 dev lo")
+    b.cmd("ip a a 10.0.0.2/32 dev lo")
+    a.cmd("ip a a 11.0.0.1/24 dev ab0")
+    a.cmd("ip a a 22.0.0.1/24 dev ab1")
+    b.cmd("ip a a 11.0.0.2/24 dev ba0")
+    b.cmd("ip a a 22.0.0.2/24 dev ba1")
+    a.cmd("ip r a default via 11.0.0.2")
+    b.cmd("ip r a default via 11.0.0.1")
+    return net
+
 def main():
-    print("R2DTWO POF test")
-    create_ifaces()
-    config_ifaces()
+    print("R2DTWO stress test")
     if len(sys.argv) == 2 and sys.argv[1] == "--debug":
         exit(1)
-    ret = 0
-    tests = [long_run]
-    for test in tests:
-        ret += test()
+    try:
+        net = create_net()
+        result = long_run(net)
+        if result == 0:
+            print("✘")
+        else:
+            print("✔")
         exec_fg("killall r2dtwo")
-    print(f'All test completed, {ret}/{len(tests)} successfully')
-    # exit(0)
-    cleanup_ifaces()
-    if ret != len(tests):
+    except KeyboardInterrupt:
+        print(" Interrupted, cleanup...")
+        exec_fg("killall ping")
+        exec_fg("killall r2dtwo")
         exit(1)
-    exit(0)
+    finally:
+        net.stop()
+        exec_fg("killall ping")
+        exec_fg("killall r2dtwo")
 
 if __name__ == "__main__":
     main()
