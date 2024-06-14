901a8d49ace2cdbfeef48cb563564d152eb804fa
Ferenc Fejes <ferenc.fejes@ericsson.com>
2024-06-14 13:13:29 +0200

    debug: Add GDB helpers
    Simple helper to print a ConfAction list implemented at first.
    Usage:
    1. Source the helpers from GDB cli or .gdbinit:
    (gdb) source doc/r2dtwo_gdb_helpers.py
    2. Use the ConfAction list head to print:
    (gdb) ca_foreach stst->actions
    ConfAction list (head: 0x555555599c60):
    (0x555555599c60): E1 C1
    (0x555555599c00): G
    (0x555555598c60): automatic TTL reduce on mpls

---
+++ doc/r2dtwo_gdb_helpers.py
@@ -0,0 +1,37 @@
+# To access the helper functions, source this file from gdb or from .gdbinit
+# (gdb) source doc/r2dtwo_gdb_helpers.py
+# Check user-defined helpers: (gdb) help user-defined
+
+import gdb
+
+class ConfActionForeach(gdb.Command):
+    """
+    Walk through a ConfAction list.
+    Usage example:
+        (gdb) ca_foreach stst->actions
+    """
+
+    def __init__(self) -> None:
+        super().__init__("ca_foreach", gdb.COMMAND_USER)
+
+    def _ca_foreach(self, val):
+        ret = f"ConfAction list (head: {val}):"
+        ca = val
+        while ca:
+            ret += f"\n({ca}): {ca['text'].string()}"
+            ca = ca["next"]
+        return ret
+
+    def complete(self, text: str, word: str) -> object:
+        return gdb.COMPLETE_SYMBOL
+
+    def invoke(self, args, from_tty):
+        node = gdb.parse_and_eval(args)
+        if str(node.type) != "struct ConfAction *":
+            print("Expected pointer argument type: ConfAction *")
+            return
+        print(self._ca_foreach(node))
+
+ConfActionForeach()
+
+# TODO: foreach HashMap helper
