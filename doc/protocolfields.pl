ee24155f0587da7b125feb084eaaf0dc5ccfe6d8
Miklós Máté <mate@tmit.bme.hu>
2024-07-06 01:52:44 +0200

    improve the protocol field documentation generator script
    Now we have separate documentation for cvlan and svlan.

--- doc/protocolfields.pl
+++ doc/protocolfields.pl
@@ -14,20 +14,33 @@ This list was generated with $0.
 EOF
 
 my $protoname;
+my %headers;
+my %protocols;
 
 while (<>) {
     if ($protoname) {
         if (/^};/) {
             undef $protoname
         } else {
-            /"(\w+)"/ and print "* `$1`";
-            /\/\/(.*)/ and print "$1";
-            print "\n";
+            my ($name) = /"(\w+)"/;
+            my ($comment) = /\/\/(.*)/;
+            $comment = '' unless defined $comment;
+            push @{$headers{$protoname}}, "* `$name` $comment\n";
         }
     } else {
         if (/^static const struct ProtocolField (\w+)_fields/) {
             $protoname = $1;
-            print "\n## $protoname\n\n"
+        }
+        if (/^    \{"(\w+)", (\w+)_fields/) {
+            $protocols{$1} = $2;
         }
     }
+}
+
+for my $proto (sort keys %protocols) {
+    my $fields = $headers{$protocols{$proto}};
+    next unless defined $fields;
+
+    print "\n## $proto\n\n";
+    print for @$fields;
 }
