9f3727b8fb7d5df58b08330443ee5609eb031279
Miklós Máté <mate@tmit.bme.hu>
2024-10-18 01:27:04 +0200

    getting started: initial dynip scenario
    With static IP for now.

--- getting_started/scenario_dynip/README.md
+++ getting_started/scenario_dynip/README.md
@@ -1,4 +1,45 @@
 # Scenario: Dynamic IP configuration
 
-TODO
+Topology:
+
+* mobile has two 5G interfaces connecting to different networks
+* both 5G networks have a gateway node
+* server has one interface toward each mobile network
+
+The traffic between the server and the mobile is replicated on both 5G networks.
+
+The address of ethX on the server and wwanX on the mobile must be routable and directly reachable from the other endpoint.
+If the provider puts the mobile behind a stateful firewall our UDP PseudoWire tunnels will not work.
+
+This topology is also a demonstration on how to set up R2DWO on an end-station.
+
+```
+ server                          gateway1                                mobile
+┌────────────────────────────┐  ┌──────────────┐  ┌────────────────────────────┐
+│           ╭───────────────╮│  │              │  │╭───────────────╮           │
+│           │           eth0││  │swp0          │  ││               │           │
+│           │            ───┼┼──┼───           │  ││               │           │
+│           │        fd11::2││  │fd11::1   swp1│  ││ wwan0         │           │
+│           │               ││  │           ───┼──┼┼───            │           │
+│           │               ││  │       fd12::1│  ││               │           │
+│           │               ││  └──────────────┘  ││               │           │
+│  vrf0     │               ││                    ││               │     vrf0  │
+│   ────────┼───            ││                    ││            ───┼────────   │
+│ fd66::1   │               ││                    ││               │   fd55::1 │
+│           │               ││   gateway2         ││               │           │
+│           │               ││  ┌──────────────┐  ││               │           │
+│           │               ││  │              │  ││               │           │
+│           │           eth1││  │swp0          │  ││               │           │
+│           │             ──┼┼──┼────          │  ││               │           │
+│           │        fd21::2││  │fd21::1   swp1│  ││wwan1          │           │
+│           │ R2DTWO        ││  │           ───┼──┼┼───     R2DTWO │           │
+│           ╰───────────────╯│  │       fd22::1│  │╰───────────────╯           │
+└────────────────────────────┘  └──────────────┘  └────────────────────────────┘
+```
+
+TODO explain the routing
+
+TODO radvd configs, try if it works
+
+TODO explain how to use this stuff
 
---
+++ getting_started/scenario_dynip/env.sh
@@ -0,0 +1,116 @@
+CNTFILE=/tmp/r2dtwo_test_env.count
+SCENNAME="scenario_dynip"
+function server() { ip netns exec server $@ ; }
+function mobile() { ip netns exec mobile $@ ; }
+function gateway1() { ip netns exec gateway1 $@ ; }
+function gateway2() { ip netns exec gateway2 $@ ; }
+export -f server
+export -f mobile
+export -f gateway1
+export -f gateway2
+
+if [ $(id -u) -ne 0 ]; then
+  echo "Usage: run 'source env.sh' as root"
+  return -1
+fi
+
+if which r2dtwo > /dev/null ; then true ; else
+  echo "r2dtwo executable not found."
+  echo "Compile and install r2dtwo first."
+  return -2
+fi
+
+configure_networkenv() {
+  echo "Initialize r2dtwo test environment"
+  # Create the test namespace
+  ip netns add server 2>/dev/null
+  ip netns add mobile 2>/dev/null
+  ip netns add gateway1 2>/dev/null
+  ip netns add gateway2 2>/dev/null
+
+  ip link add swp0 netns gateway1 type veth peer eth0 netns server
+  ip link add swp0 netns gateway2 type veth peer eth1 netns server
+  ip link add swp1 netns gateway1 type veth peer wwan0 netns mobile
+  ip link add swp1 netns gateway2 type veth peer wwan1 netns mobile
+
+  ip link add vrf0 netns server type vrf table 254
+  ip link add vrf0 netns mobile type vrf table 254
+
+  # Configure the test environment inside the namespace
+  server ip link set dev lo up
+  mobile ip link set dev lo up
+  gateway1 ip link set dev lo up
+  gateway2 ip link set dev lo up
+
+  server ip link set eth0 mtu 1600 up
+  server ip link set eth1 mtu 1600 up
+  mobile ip link set wwan0 mtu 1600 up
+  mobile ip link set wwan1 mtu 1600 up
+  gateway1 ip link set dev swp0 mtu 1600 up
+  gateway1 ip link set dev swp1 mtu 1600 up
+  gateway2 ip link set dev swp0 mtu 1600 up
+  gateway2 ip link set dev swp1 mtu 1600 up
+  server ip link set vrf0 up
+  mobile ip link set vrf0 up
+
+  # Configure the addresses
+  server ip address add fd11::2/64 dev eth0
+  server ip address add fd21::2/64 dev eth1
+  gateway1 ip address add fd11::1/64 dev swp0
+  gateway2 ip address add fd21::1/64 dev swp0
+
+  gateway1 ip address add fd12::1/64 dev swp1
+  gateway2 ip address add fd22::1/64 dev swp1
+  mobile ip address add fd12::2/64 dev wwan0
+  mobile ip address add fd22::2/64 dev wwan1
+
+  server ip address add fd55::1/64 dev vrf0
+  mobile ip address add fd66::1/64 dev vrf0
+
+  # Enable IP forwarding
+  gateway1 sysctl -w net.ipv4.ip_forward=1
+  gateway1 sysctl -w net.ipv6.conf.all.forwarding=1
+  gateway2 sysctl -w net.ipv4.ip_forward=1
+  gateway2 sysctl -w net.ipv6.conf.all.forwarding=1
+
+  # Configure routing
+  server ip route add fd10::/12 via fd11::1 dev eth0
+  server ip route add fd20::/12 via fd21::1 dev eth1
+  mobile ip route add fd10::/12 via fd12::1 dev wwan0
+  mobile ip route add fd20::/12 via fd22::1 dev wwan1
+
+  server ip route add fd66::/64 dev vrf0
+  mobile ip route add fd55::/64 dev vrf0
+
+}
+
+# This is totally unsafe
+# TODO: use flock to avoid races
+if [ -f "$CNTFILE" ]; then
+  read scenname cntvalue < $CNTFILE
+  if [ "$scenname" != "$SCENNAME" ] ; then
+    echo "scenario '$scenname' is already running, stop it before starting $SCENNAME"
+    return -2
+  fi
+  newvalue=`expr $cntvalue + 1`
+  echo "$SCENNAME $newvalue" > $CNTFILE
+else
+  echo "$SCENNAME 1" > $CNTFILE
+  configure_networkenv
+fi
+
+/bin/bash --init-file <(echo "PS1='(dynip) \u:\W# '")
+
+read scenname cntvalue < $CNTFILE
+if [ $cntvalue -eq 1 ]; then #last bash instance in the env, do cleanup
+  echo "Cleanup r2dtwo test environment"
+  rm $CNTFILE
+  #Cleanup the test namespace
+  ip netns del server 2>/dev/null
+  ip netns del mobile 2>/dev/null
+  ip netns del gateway1 2>/dev/null
+  ip netns del gateway2 2>/dev/null
+else
+  newvalue=`expr $cntvalue - 1`
+  echo "$SCENNAME $newvalue" > $CNTFILE
+fi
