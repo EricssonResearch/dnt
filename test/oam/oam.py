529c92fe79e58a2ac39aa7c21d080ecf46a0a3f6
István Moldován <zoltmsi@xead.ericsson.com>
2025-03-21 17:25:27 +0100

    Ask user whether start r2dtwos or not in debug mode.

--- test/oam/oam.py
+++ test/oam/oam.py
@@ -384,7 +384,9 @@ def main():
         net = create_net()
         config_net(net)
         if debug:
-            start_r2dtwos(net, False)
+            inp = input("Do you want ti start R2DWOs? (yes/no): ")
+            if inp.lower() in ["yes", "y"]:
+                start_r2dtwos(net, False)
             CLI(net)
         else:
             all_ok = run_tests(net, testcases)
